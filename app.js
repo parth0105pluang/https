a504488023a2670f6783e1d60ef5fe454915ee52
Parth Sharma <parthsharma@Parths-MacBook-Pro.local>
2022-01-22 14:25:07 +0530

    using req.url

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const http = require('http');
 const server = http.createServer((req,res)=>{
-console.log(req)
-res.write('Welcome to homepage')
-res.end()
+//console.log(req)
+//res.write('Welcome to homepage')
+//res.end()
+if(req.url==='/')
+{
+    res.end('Homepage')
+}
+if(req.url==='/about')
+{
+    res.end('About')
+}
 })
 server.listen(8000)
